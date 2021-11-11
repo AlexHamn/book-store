942ff72cea316e7a6be21ecfc7756e76d9b7927a
AlexHamn <agalexandro16@gmail.com>
2021-11-11 08:50:13 -0600

    initialize oriject with components

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+import Navbar from './components/Navbar';
+
+const App = () => (
+  <Router>
+    <header>
+      <h1>Bookstore CMS</h1>
+      <Navbar />
+    </header>
+    <Switch>
+      <Route exact path="/Books">
+        <Books />
+      </Route>
+      <Route exact path="/Categories">
+        <Categories />
+      </Route>
+    </Switch>
+  </Router>
+);
+
+export default App;
